8846204ac79d5a5841ab0f9faa731be867f0775f
AKRKPROMOTORA\maicon.costa <maicon.costa@tecksolucoes.com.br>
2025-10-01 12:29:51 -0300

    hotfix: Correção de click no componente MButton

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    <MButton>Teste</MButton>
-  </div>
+  <v-row>
+    <v-col>
+      <MButton @click="() => console.log('Teste')">Teste</MButton>
+    </v-col>
+  </v-row>
 </template>
-
-<script setup>
-</script>
