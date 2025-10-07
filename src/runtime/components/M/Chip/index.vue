940d3cdffba1efeb59dbf1adf51516dac2140764
AKRKPROMOTORA\maicon.costa <maicon.costa@tecksolucoes.com.br>
2025-10-07 11:57:20 -0300

    feat: Novo componente MChip

---
+++ src/runtime/components/M/Chip/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-chip v-bind="props">
+        <template v-for="name in slots" :key="name">
+            <slot :name="name" v-if="$slots[name]"></slot>
+        </template>
+    </v-chip>
+</template>
+
+<script lang="ts" setup>
+import { IMChip } from '@/types/MChip';
+
+const slots = ["append", "close", "default", "filter", "label", "prepend"];
+
+const props = withDefaults(defineProps<IMChip>(), ({
+  density: 'default',
+  ripple: true,
+  size: 'default',
+  tag: 'span',
+  variant: 'tonal'
+}))
+</script>
